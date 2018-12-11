9528709da27d265ff43b138c620adebec8f374c4
Daan Ferdinandusse <dferdinandusse@gmail.com>
2018-12-11 12:13:40 +0100

    initial

---
+++ app/src/main/java/com/example/daan/trivia/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.daan.trivia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void getHighscores(View view) {
+    }
+
+    public void play(View view) {
+    }
+}
